b8ec21176baefb58cf7df63e2792ef5ed8e7e4d8
Egor Shkarin <58490727+Grandschtien@users.noreply.github.com>
2022-06-02 21:00:30 +0300

    Добавил отчет к НИР

--- NIR/Model/ImagePredictor.swift
+++ NIR/Model/ImagePredictor.swift
@@ -10,56 +10,28 @@ import UIKit
 import Vision
 
 final class ImagePredictor {
-    /// - Tag: name
     static func createImageClassifier() -> VNCoreMLModel {
-        // Use a default model configuration.
         let defaultConfig = MLModelConfiguration()
-
-        // Create an instance of the image classifier's wrapper class.
         let imageClassifierWrapper = try? GenderClassifier_1(configuration: defaultConfig)
-
         guard let imageClassifier = imageClassifierWrapper else {
             fatalError("App failed to create an image classifier model instance.")
         }
-
-        // Get the underlying model instance.
         let imageClassifierModel = imageClassifier.model
-
-        // Create a Vision instance using the image classifier's model instance.
         guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
             fatalError("App failed to create a VNCoreMLModel instance.")
         }
 
         return imageClassifierVisionModel
     }
-
-    /// A common image classifier instance that all Image Predictor instances use to generate predictions.
-    ///
-    /// Share one `VNCoreMLModel` instance --- for each Core ML model file --- across the app,
-    /// since each can be expensive in time and resources.
     private static let imageClassifier = createImageClassifier()
-
-    /// Stores a classification name and confidence for an image classifier's prediction.
-    /// - Tag: Prediction
     struct Prediction {
-        /// The name of the object or scene the image classifier recognizes in an image.
         let classification: String
-
-        /// The image classifier's confidence as a percentage string.
-        ///
-        /// The prediction string doesn't include the % symbol in the string.
         let confidencePercentage: String
     }
-
-    /// The function signature the caller must provide as a completion handler.
     typealias ImagePredictionHandler = (_ predictions: [Prediction]?) -> Void
-
-    /// A dictionary of prediction handler functions, each keyed by its Vision request.
     private var predictionHandlers = [VNRequest: ImagePredictionHandler]()
 
-    /// Generates a new request instance that uses the Image Predictor's image classifier model.
     private func createImageClassificationRequest() -> VNImageBasedRequest {
-        // Create an image classification request with an image classifier model.
 
         let imageClassificationRequest = VNCoreMLRequest(model: ImagePredictor.imageClassifier,
                                                          completionHandler: visionRequestHandler)
@@ -68,9 +40,6 @@ final class ImagePredictor {
         return imageClassificationRequest
     }
 
-    /// Generates an image classification prediction for a photo.
-    /// - Parameter photo: An image, typically of an object or a scene.
-    /// - Tag: makePredictions
     func makePredictions(for photo: UIImage, completionHandler: @escaping ImagePredictionHandler) throws {
         let orientation = CGImagePropertyOrientation(photo.imageOrientation)
 
@@ -86,44 +55,32 @@ final class ImagePredictor {
         try handler.perform(requests)
     }
     private func visionRequestHandler(_ request: VNRequest, error: Error?) {
-        // Remove the caller's handler from the dictionary and keep a reference to it.
         guard let predictionHandler = predictionHandlers.removeValue(forKey: request) else {
             fatalError("Every request must have a prediction handler.")
         }
 
-        // Start with a nil value in case there's a problem.
         var predictions: [Prediction]? = nil
 
-        // Call the client's completion handler after the method returns.
         defer {
-            // Send the predictions back to the client.
             predictionHandler(predictions)
         }
 
-        // Check for an error first.
         if let error = error {
             print("Vision image classification error...\n\n\(error.localizedDescription)")
             return
         }
 
-        // Check that the results aren't nil.
         if request.results == nil {
             print("Vision request had no results.")
             return
         }
 
-        // Cast the request's results as an VNClassificationObservation array.
         guard let observations = request.results as? [VNClassificationObservation] else {
-            // Image classifiers, like MobileNet, only produce classification observations.
-            // However, other Core ML model types can produce other observations.
-            // For example, a style transfer model produces VNPixelBufferObservation instances.
             print("VNRequest produced the wrong result type: \(type(of: request.results)).")
             return
         }
 
-        // Create a prediction array from the observations.
         predictions = observations.map { observation in
-            // Convert each observation into an ImagePredictor.Prediction instance.
             Prediction(classification: observation.identifier,
                        confidencePercentage: observation.confidencePercentageString)
         }
@@ -132,7 +89,6 @@ final class ImagePredictor {
 
 
 extension VNClassificationObservation {
-    /// Generates a string of the observation's confidence as a percentage.
     var confidencePercentageString: String {
         let percentage = confidence * 100
 
@@ -153,10 +109,6 @@ extension VNClassificationObservation {
 import ImageIO
 
 extension CGImagePropertyOrientation {
-    /// Converts an image orientation to a Core Graphics image property orientation.
-    /// - Parameter orientation: A UIImage.Orientation instance.
-    ///
-    /// The two orientation types use different raw values.
     init(_ orientation: UIImage.Orientation) {
         switch orientation {
             case .up: self = .up
